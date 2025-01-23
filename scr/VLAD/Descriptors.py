b26efa2702045e9a4ed8f824d6ca0d39ed79f20b
ginofft <ntnghia.andrei@gmail.com>
2025-01-23 14:19:44 +1100

    clean the repo

--- scr/VLAD/Descriptors.py
+++
@@ -1,6 +0,0 @@
-import cv2
-
-def compute_SIFT(img):
-  sift = cv2.SIFT_create()
-  kp, des = sift.detectAndCompute(img,None)
-  return des
--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup , find_packages
-import pathlib
-
-setup(
-    name='VLAD',
-    version='0.1.2',
-    package_dir={"":"scr"},
-    packages=find_packages(where='scr')
-)
